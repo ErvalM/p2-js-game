cc96d2478c578e34c48f176ac406bc33a7bc71c2
unknown <ervalmachado@yahoo.com>
2023-02-20 18:00:31 +0800

    feat: import / export js for questions (Dry code), added class in questions

---
+++ p2-js-game/questions.js
@@ -0,0 +1,19 @@
+class Question{
+    constructor(question, response, answer){
+        this.question = question;
+        this.response = response;
+        this.answer = answer;
+    }
+}
+
+export let questions = [];
+questions.push(new Question('The maker of this game is Erval.','yes yoh!', true))
+questions.push(new Question('You can use semicolon (;) at the beginning of a line.','Yes, you can', true))
+questions.push(new Question('NaN (not a number) is not a number.','Nan (not a number) is a number.', false))
+questions.push(new Question('JavaScript is also known as Jscript.','Jscript is a relative of JavaScript.', false))
+questions.push(new Question('The (alt) attribute of the img tag in HTML is mandatory','Yes, it is mandatory', true))
+questions.push(new Question('CSS stands for Cascading Style Ship.','Nope, CSS stands for Cascading Style Sheet', false))
+questions.push(new Question('this is an inline function: let func = function(){};.','Yes, it is an inline function', true))
+questions.push(new Question('This is an anonymous function: function func(){};.','Nope, it is not', false))
+questions.push(new Question('Mocha was JavaScript\'s original name.','Yes sir! Thankfully, this name didn\'t stick around for long!', true))
+questions.push(new Question('JavaScript was created in just 10 days.','Awesome right? Eich created JavaScript in just 10 days, so what is your excuse?', true))
