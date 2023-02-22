c3bbe7be5894cd2ea5b0da6cc01ff895da01fec0
unknown <ervalmachado@yahoo.com>
2023-02-22 21:25:32 +0800

    fix: question index repeating, score, answer identifier

--- p2-js-game/gamePractice.js
+++ p2-js-game/gamePractice.js
@@ -4,19 +4,22 @@ console.log(questions[1])
 //to do call on randomized questions 
 
 /* HTML to JS */
-const trueBtn = document.querySelector('.true');
-const falseBtn = document.querySelector('.false');
+const trueBtn = document.querySelector('.true')
+const falseBtn = document.querySelector('.false')
 const nextQuestionBtn = document.querySelector('#nextquestion')
 const userScore = document.querySelector('.scores')
 const questionResponse = document.querySelector('#questionResponse')
 const questionOutput = document.querySelector('.questiontext')
-const restartButton = document.querySelectorAll('.restartButton')
+const resetBtn = document.querySelector('#reset')
 
 /* JS to HTML */
 let counter = 0
-let questionCounter;
+let questionCounter = 0 
+let roundNumber = 1
 let questionHeader = document.createElement('h3')
 questionOutput.appendChild(questionHeader)
+questionResponse.style.backgroundColor = '#BCE29E'
+questionResponse.style.fontSize = '25px'
 
 // for(let i = 0; i < questions.length; i++){
 //     questions[i] 
@@ -29,7 +32,12 @@ function answerCheck(check){
     trueBtn.classList.add('disabled')
     falseBtn.classList.add('disabled')
     nextQuestionBtn.classList.remove('hide')
-    if(check == true){
+    roundNumber++
+        console.log(roundNumber)
+    if(roundNumber === 30){
+        gameOver()
+    }
+    if(check == questions[questionCounter].answer){
         counter++
         userScore.innerHTML = counter 
     }
@@ -38,30 +46,25 @@ function answerCheck(check){
 
 function randomQuestions() {
     for (let i = 0; i < questions.length; i++) {
-        let j = Math.floor(Math.random() * questions.length);
+        let questionIndex = Math.floor(Math.random() * questions.length);
         let temp = questions[i];
-        questions[i] = questions[j];
-        questions[j] = temp;
-        questionCounter = j
-        questionHeader.innerHTML = questions[j].question
+        questions[i] = questions[questionIndex];
+        questions[questionIndex] = temp;
+        questionCounter = questionIndex
+        questionHeader.innerHTML = questions[questionIndex].question
     }
     console.log(questions);
 }
 randomQuestions()
 
-/* buttons */
-trueBtn.addEventListener('click', function(){
-    answerCheck(true);
-});
-
-falseBtn.addEventListener('click', function(){
-    answerCheck(false);
-});
-
 nextQuestionBtn.addEventListener('click', function(){
-    if (questionCounter < questions.length - 1){
+    if (questionCounter == 29){
+        questionCounter = 0
+    }
+    if (questionCounter < questions.length -1){
         questionCounter++
     }
+    console.log(questionCounter)
     console.log(questions[questionCounter].question)
     questionHeader.textContent = questions[questionCounter].question;
     questionResponse.textContent = ''
@@ -70,10 +73,23 @@ nextQuestionBtn.addEventListener('click', function(){
     falseBtn.classList.remove('disabled')
 });
 
-restartButton.addEventListener('click', function(){
-   document.location.reload(true);
+/* buttons */
+trueBtn.addEventListener('click', function(){
+    answerCheck('true');
+});
 
+falseBtn.addEventListener('click', function(){
+    answerCheck('false');
+});
+
+resetBtn.addEventListener('click', function(){
+   location.reload()
 })
+
+
+function gameOver(){
+    alert ('game over')
+}
 
 
 
@@ -85,3 +101,9 @@ restartButton.addEventListener('click', function(){
 //button for restart (with troll message)
 
 //design last
+
+
+//bug:
+//after few next question, question repeats endlessly.
+//false doesn't give score, only true button regardless if the answer is right or wrong
+//restart button not working 
